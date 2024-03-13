ec1ee32572cd4f23b385f5d8998727e2a03445fc
Dylan Clerckx <dylanckx@gmail.com>
2024-03-13 16:22:20 +0100

    réparation dernière prestation

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,52 +1,47 @@
-import PropTypes from "prop-types";
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import PropTypes from 'prop-types';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 
 const DataContext = createContext({});
 
 export const api = {
-  loadData: async () => {
-    const json = await fetch("/events.json");
-    return json.json();
-  },
+	loadData: async () => {
+		const json = await fetch('/events.json');
+		return json.json();
+	},
 };
 
 export const DataProvider = ({ children }) => {
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
-  const getData = useCallback(async () => {
-    try {
-      setData(await api.loadData());
-    } catch (err) {
-      setError(err);
-    }
-  }, []);
-  useEffect(() => {
-    if (data) return;
-    getData();
-  });
-  
-  return (
-    <DataContext.Provider
-      // eslint-disable-next-line react/jsx-no-constructed-context-values
-      value={{
-        data,
-        error,
-      }}
-    >
-      {children}
-    </DataContext.Provider>
-  );
+	const [error, setError] = useState(null);
+	const [data, setData] = useState(null);
+	const last = data?.events?.sort((evtA, evtB) => (new Date(evtA.date) > new Date(evtB.date) ? -1 : 1))[0];
+	const getData = useCallback(async () => {
+		try {
+			setData(await api.loadData());
+		} catch (err) {
+			setError(err);
+		}
+	}, []);
+	useEffect(() => {
+		if (data) return;
+		getData();
+	});
+
+	return (
+		<DataContext.Provider
+			// eslint-disable-next-line react/jsx-no-constructed-context-values
+			value={{
+				data,
+				error,
+				last,
+			}}>
+			{children}
+		</DataContext.Provider>
+	);
 };
 
 DataProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+	children: PropTypes.node.isRequired,
+};
 
 export const useData = () => useContext(DataContext);
 
