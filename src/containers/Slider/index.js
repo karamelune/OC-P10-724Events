ddad0e990b63f5a31116c9a9e2e3d9184c61444f
Dylan Clerckx <dylanckx@gmail.com>
2024-05-23 11:36:23 +0200

    correction formatage pour + de lisibilité

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,46 +5,56 @@ import { getMonth } from '../../helpers/Date';
 import './style.scss';
 
 const Slider = () => {
-	const { data } = useData();
-	const [index, setIndex] = useState(0);
-	const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) > new Date(evtB.date) ? -1 : 1)) || [];
-	const nextCard = () => {
-		setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
-	};
+    const { data } = useData();
+    const [index, setIndex] = useState(0);
+    const byDateDesc =
+        data?.focus.sort((evtA, evtB) =>
+            new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+        ) || [];
+    const nextCard = () => {
+        setTimeout(
+            () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+            5000
+        );
+    };
 
-	useEffect(() => {
-		nextCard();
-	});
+    useEffect(() => {
+        nextCard();
+    });
 
-	return (
-		<div className="SlideCardList">
-			{byDateDesc?.map((event, idx) => (
-				<div key={event.title} className={`SlideCard SlideCard--${index === idx ? 'display' : 'hide'}`}>
-					<img src={event.cover} alt="forum" />
-					<div className="SlideCard__descriptionContainer">
-						<div className="SlideCard__description">
-							<h3>{event.title}</h3>
-							<p>{event.description}</p>
-							<div>{getMonth(new Date(event.date))}</div>
-						</div>
-					</div>
-				</div>
-			))}
-			<div className="SlideCard__paginationContainer">
-				<div className="SlideCard__pagination">
-					{byDateDesc.map((event, radioIdx) => (
-						<input
-							key={event.title}
-							type="radio"
-							name="radio-button"
-							checked={index === radioIdx}
-							readOnly
-						/>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="SlideCardList">
+            {byDateDesc?.map((event, idx) => (
+                <div
+                    key={event.title}
+                    className={`SlideCard SlideCard--${
+                        index === idx ? 'display' : 'hide'
+                    }`}>
+                    <img src={event.cover} alt="forum" />
+                    <div className="SlideCard__descriptionContainer">
+                        <div className="SlideCard__description">
+                            <h3>{event.title}</h3>
+                            <p>{event.description}</p>
+                            <div>{getMonth(new Date(event.date))}</div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div className="SlideCard__paginationContainer">
+                <div className="SlideCard__pagination">
+                    {byDateDesc.map((event, radioIdx) => (
+                        <input
+                            key={event.title}
+                            type="radio"
+                            name="radio-button"
+                            checked={index === radioIdx}
+                            readOnly
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Slider;
--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,46 +1,54 @@
 import PropTypes from 'prop-types';
-import { createContext, useCallback, useContext, useEffect, useState } from 'react';
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from 'react';
 
 const DataContext = createContext({});
 
 export const api = {
-	loadData: async () => {
-		const json = await fetch('/events.json');
-		return json.json();
-	},
+    loadData: async () => {
+        const json = await fetch('/events.json');
+        return json.json();
+    },
 };
 
 export const DataProvider = ({ children }) => {
-	const [error, setError] = useState(null);
-	const [data, setData] = useState(null);
-	const last = data?.events?.sort((evtA, evtB) => (new Date(evtA.date) > new Date(evtB.date) ? -1 : 1))[0];
-	const getData = useCallback(async () => {
-		try {
-			setData(await api.loadData());
-		} catch (err) {
-			setError(err);
-		}
-	}, []);
-	useEffect(() => {
-		if (data) return;
-		getData();
-	});
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const last = data?.events?.sort((evtA, evtB) =>
+        new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    )[0];
+    const getData = useCallback(async () => {
+        try {
+            setData(await api.loadData());
+        } catch (err) {
+            setError(err);
+        }
+    }, []);
+    useEffect(() => {
+        if (data) return;
+        getData();
+    });
 
-	return (
-		<DataContext.Provider
-			// eslint-disable-next-line react/jsx-no-constructed-context-values
-			value={{
-				data,
-				error,
-				last,
-			}}>
-			{children}
-		</DataContext.Provider>
-	);
+    return (
+        <DataContext.Provider
+            // eslint-disable-next-line react/jsx-no-constructed-context-values
+            value={{
+                data,
+                error,
+                last,
+            }}>
+            {children}
+        </DataContext.Provider>
+    );
 };
 
 DataProvider.propTypes = {
-	children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 export const useData = () => useContext(DataContext);
